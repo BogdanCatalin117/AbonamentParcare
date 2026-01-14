b9e28b438685d145c860b328f93058aebe97c73c
Albert Zava <albertzavalas123@gmail.com>
2026-01-14 17:55:25 +0200

    Add files via upload

---
+++ Ilogger.cs
@@ -0,0 +1,9 @@
+﻿namespace Proiect;
+
+public interface Ilogger
+{
+    public void Eroare(string mesaj);
+    public void Warn(string mesaj);
+    public void Info(string mesaj);
+    public void DelLog();
+}
